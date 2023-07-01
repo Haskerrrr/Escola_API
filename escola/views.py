7c74b747471ebdb9cb9b20b622d642633cf8b082
Gusta <gustavorodrigues182@live.com>
2023-07-01 04:26:13 -0300

    CursoViewSet

--- escola/views.py
+++ escola/views.py
@@ -6,3 +6,8 @@ class AlunosViewSet(viewsets.ModelViewSet):
     #Exibindo todos os alunos e alunas
     queryset = Aluno.objects.all()
     serializer_class = AlunoSerializer
+    
+class CursosViewSet(viewsets.ModelViewSet):
+    #Exibindo todos os cursos
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
