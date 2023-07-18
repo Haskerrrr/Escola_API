dc12e0013a9cc01183a4c4835a189bf6e800570c
Gusta <gustavorodrigues182@live.com>
2023-07-18 03:22:59 -0300

    Fix bugs e proxima aula

--- escola/views.py
+++ escola/views.py
@@ -1,18 +1,25 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from escola.models import Aluno, Curso, Matricula
-from escola.serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer
+from escola.serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunoSerializer
 
 class AlunosViewSet(viewsets.ModelViewSet):
-    #Exibindo todos os alunos e alunas
+    """Exibindo todos os alunos e alunas"""
     queryset = Aluno.objects.all()
     serializer_class = AlunoSerializer
 
 class CursosViewSet(viewsets.ModelViewSet):
-    #Exibindo todos os cursos
+    """Exibindo todos os cursos"""
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
 
 class MatriculaViewSet(viewsets.ModelViewSet):
-    #Listando todos os cursos
+    """Listando todos os cursos"""
     queryset = Matricula.objects.all()
     serializer_class = MatriculaSerializer
+
+class ListaMatriculasAluno(generics.ListAPIView):
+    """Listando as matrículas de um aluno ou aluna"""
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(aluno_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculasAlunoSerializer
--- setup/urls.py
+++ setup/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from escola.views import AlunosViewSet, CursosViewSet, MatriculaViewSet
+from escola.views import AlunosViewSet, CursosViewSet, MatriculaViewSet, ListaMatriculasAluno
 from rest_framework import routers
 
 #Registrar rotas
@@ -11,5 +11,6 @@ router.register('matriculas', MatriculaViewSet, basename='Matriculas')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls))
+    path('', include(router.urls)), 
+    path('aluno/<int:pk>/matriculas/', ListaMatriculasAluno.as_view())
 ]
